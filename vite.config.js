7d47da65794a6e02335afb45cbfe6ab331257a22
xun.hainan <xun.hainan@zol.com.cn>
2026-05-03 16:32:08 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    target: 'modules', //默认modules，如果需要兼容旧浏览器，可以设置为es2015或更低
+    rollupOptions: {
+
+    }
+  }
+})
